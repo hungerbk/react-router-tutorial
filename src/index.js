3549ea380791bd0a530699b0c44e49bfe496afb3
hungerbk <wjantlr@gmail.com>
2023-11-13 11:07:54 +0900

    feat: userParams ,useSearchParams와 useNavigate

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import ErrorNotFound from "./pages/ErrorNotFound";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Greeting from "./pages/Greeting";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "/about", element: <About /> },
+      { path: "/greet/:name", element: <Greeting /> },
     ],
   },
   // { path: "/about", element: <About />, errorElement: <ErrorNotFound /> },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Home() {
-  return <>Home</>;
+  const [params, setParams] = useState("");
+  const [queryParamsInput, setQueryParamsInput] = useState("");
+  const [queryParams, setQueryParams] = useSearchParams();
+
+  // 1 리액트 라우터에서는 Link
+  // 2 Navigate
+  const navigate = useNavigate();
+
+  const handleParamsChange = (e) => {
+    setParams(e.target.value);
+  };
+
+  const handleQueryParamsInputChange = (e) => {
+    setQueryParamsInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    navigate(`/greet/${params}`);
+    setQueryParams({ to: queryParamsInput });
+  };
+
+  return (
+    <div>
+      <input type="text" value={params} onChange={handleParamsChange} placeholder="search params" />
+      <button onClick={handleClick}>환영!</button>
+      <input type="text" value={queryParamsInput} onChange={handleQueryParamsInputChange} placeholder="search query params" />
+    </div>
+  );
 }
